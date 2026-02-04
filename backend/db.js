4f6165cc4a210b14d66e5d2dbcd732dda8c61ed2
labanti <100425522+Jlp06@users.noreply.github.com>
2026-02-04 23:39:10 +0530

    update db.js for render deployment

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "hospital_queue",
-    password: "12345",
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 module.exports = pool;
+
